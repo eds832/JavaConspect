d5e1ac1dc1468cb65e8f0009b9a97cc4eaeb34a1
Eduard <eds832@gmail.com>
2018-01-09 14:06:35 +0300

    canged ClassFileTest.java

--- ClassFileTest.java
+++ ClassFileTest.java
@@ -10,9 +10,15 @@ public class ClassFileTest {
 	}
 	public static void main(String [] args) {
 		class StaticMethodClass { }
+		new Object() {
+			public String toString() { return "str"; }
+		};
 	}
 	public void method() {
 		class MetodClass { }
+		new Object() {
+			public String toString() { return "str"; }
+		};
 	}
 	public static class NestedClass { }
 	public class InnerClass { }
